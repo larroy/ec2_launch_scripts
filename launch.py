3342d0047f5d932224a19091cbc411675eb3fec8
Pedro Larroy <pllarroy@amazon.com>
2018-12-11 14:03:42 +0100

    Automatic ubuntu AMI selection

--- launch.py
+++ launch.py
@@ -12,7 +12,27 @@ from os.path import expanduser
 from subprocess import call, check_call
 import botocore
 import yaml
+import urllib.request
+import re
 
+
+def get_ubuntu_ami(region, release, arch='amd64', instance_type='hvm:ebs-ssd'):
+    # https://aws.amazon.com/amazon-linux-ami/instance-type-matrix/
+    # https://cloud-images.ubuntu.com/locator/ec2/  -> Js console -> Network
+    ami_list = yaml.safe_load(urllib.request.urlopen("https://cloud-images.ubuntu.com/locator/ec2/releasesTable").read())['aaData']
+    # Items look like:
+    #['us-east-1',
+    # 'artful',
+    # '17.10',
+    # 'amd64',
+    # 'hvm:instance-store',
+    # '20180621',
+    # '<a href="https://console.aws.amazon.com/ec2/home?region=us-east-1#launchAmi=ami-71e2b40e">ami-71e2b40e</a>',
+    # 'hvm']
+    res = [x for x in ami_list if x[0] == 'eu-west-1' and x[2].startswith(release) and x[3] == arch and x[4] == instance_type]
+    ami_link = res[0][6]
+    ami_id = re.sub('<[^<]+?>', '', ami_link)
+    return ami_id
 
 def wait_port_open(server, port, timeout=None):
     """ Wait for network service to appear
@@ -154,6 +174,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="launcher")
     parser.add_argument('-n', '--instance-name', default=launch_template.get('instance-name', "{}-{}".format('worker', getpass.getuser())))
     parser.add_argument('-i', '--instance-type', default=launch_template['instance-type'])
+    parser.add_argument('--ubuntu', default=launch_template['ubuntu'])
     parser.add_argument('-u', '--username', default=launch_template['username'])
     ssh_key = launch_template.get('ssh-key', os.path.join(expanduser("~"),".ssh","id_rsa.pub"))
     parser.add_argument('--ssh-key-file', default=ssh_key)
@@ -203,8 +224,9 @@ def main():
 
     args = parse_args()
 
-    for f in glob.glob("*.tfstate"):
-        os.remove(f)
+    boto3_session = boto3.session.Session()
+    current_region = boto3_session.region_name
+    logging.info("AWS Region is %s", current_region)
 
     instance_name = input("instance_name [{}]: ".format(args.instance_name))
     if not instance_name:
@@ -219,9 +241,22 @@ def main():
         ssh_key_file = args.ssh_key_file
     assert os.path.isfile(ssh_key_file)
 
-    ami = input("ami [{}]: ".format(args.ami))
-    if not ami:
-        ami = args.ami
+    ubuntu = input("ubuntu release such as '18.04' (or specific 'ami') [{}]: ".format(args.ubuntu))
+    if not ubuntu:
+        ubuntu = args.ubuntu
+
+    if ubuntu.startswith('ami') or ubuntu.startswith('aki'):
+        args.ami = ubuntu
+        ubuntu = None
+
+    if not ubuntu:
+        ami = input("ami [{}]: ".format(args.ami))
+        if not ami:
+            ami = args.ami
+    else:
+        ami = get_ubuntu_ami(current_region, ubuntu)
+        logging.info("Automatic Ubuntu ami selection based on region %s and release %s -> AMI id: %s",
+                     current_region, ubuntu, ami)
 
     username = input("user name [{}]: ".format(args.username))
     if not username:
