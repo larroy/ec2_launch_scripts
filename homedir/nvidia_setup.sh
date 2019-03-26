67f96b3e2e4b92821577a976b0cbb620bf6700fd
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-03-26 11:43:47 -0700

    Update installation of loca cuda 10

--- homedir/nvidia_setup.sh
+++ homedir/nvidia_setup.sh
@@ -19,3 +19,9 @@ sudo apt-get update
 sudo apt-get -y install nvidia-driver-418
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
+
+
+wget "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb"
+apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
+apt-get update
+apt-get install cuda
