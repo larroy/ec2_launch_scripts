e0fe586d4b3c9a0131bb88f84f311501dd107a3a
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-06-11 16:45:35 -0700

    add -y

--- homedir/nvidia_setup.sh
+++ homedir/nvidia_setup.sh
@@ -29,4 +29,4 @@ wget "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-
 sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get -y install cuda
