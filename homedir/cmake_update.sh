8b3e758b41c430d9f5dc4f714f9c01cfb0425401
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-07-31 16:49:35 -0700

    Add ssl support for cmake

--- homedir/cmake_update.sh
+++ homedir/cmake_update.sh
@@ -10,7 +10,7 @@ cd ~/tmp
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
-./bootstrap
+./bootstrap --system-curl --parallel=$(nproc)
 make -j$(nproc)
 sudo make install
 
