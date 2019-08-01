768b1901ed08a4ce1cfa1ded2b6835faaf6ef0cd
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-08-01 11:36:11 -0700

    Fix sudo dpkg for nvidia setup

--- homedir/nvidia_setup.sh
+++ homedir/nvidia_setup.sh
@@ -32,4 +32,4 @@ sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
-dpkg -i libcudnn7*
+sudo dpkg -i libcudnn7*
