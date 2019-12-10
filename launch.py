ef04cf5ec2fbd45df73a1a4e91eb45cb919c97ea
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-12-10 00:10:58 -0800

    Arm 64 instances

--- launch.py
+++ launch.py
@@ -17,7 +17,7 @@ import re
 import ssl
 
 
-def get_ubuntu_ami(region, release, arch='amd64', instance_type='hvm:ebs-ssd'):
+def get_ubuntu_ami(region, release, arch='arm64', instance_type='hvm:ebs-ssd'):
     # https://aws.amazon.com/amazon-linux-ami/instance-type-matrix/
     # https://cloud-images.ubuntu.com/locator/ec2/  -> Js console -> Network
     ssl._create_default_https_context = ssl._create_unverified_context
